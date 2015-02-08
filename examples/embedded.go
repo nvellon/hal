6fb2ee16425168796b1bc21075259c77a4511aba
Nicolas Vellon <nvellon@gmail.com>
2015-02-08 11:11:44 -0300

    Embedded resources example

---
+++ examples/embedded.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/nvellon/hal"
+)
+
+type (
+	Category struct {
+		Code int
+		Name string
+	}
+
+	Product struct {
+		Code     int
+		Name     string
+		Price    int
+		Category Category
+	}
+)
+
+func (p Product) GetMap() hal.Entry {
+	return hal.Entry{
+		"name":  p.Name,
+		"price": p.Price,
+	}
+}
+
+func (c Category) GetMap() hal.Entry {
+	return hal.Entry{
+		"name": c.Name,
+	}
+}
+
+func main() {
+	c := Category{
+		Code: 1,
+		Name: "Some Category",
+	}
+
+	p := Product{
+		Code:     1,
+		Name:     "Some Product",
+		Price:    10,
+		Category: c,
+	}
+
+	// Creating HAL Resources
+	pr := hal.NewResource(p, "/products/1")
+	cr1 := hal.NewResource(p.Category, "/categories/1")
+	cr2 := hal.NewResource(p.Category, "/categories/2")
+
+	// Embeding categories into product
+	pr.Embed("categories", cr1)
+	pr.Embed("categories", cr2)
+
+	// JSON Encoding
+	j, err := json.MarshalIndent(pr, "", "  ")
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	fmt.Printf("%s", j)
+}
